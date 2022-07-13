c47c2bbd243363d192f308048a88d9b88215503f
Eity2021 <Eity2021akter@gmail.com>
2022-07-14 03:18:41 +0600

    registration page design complete

--- src/Car-services/Registration/Registration.js
+++ src/Car-services/Registration/Registration.js
@@ -1,17 +1,55 @@
 import React from "react";
+import { Form } from "react-bootstrap";
 import { Link } from "react-router-dom";
-
+import login from "../images/log.jpg";
+import Button from 'react-bootstrap/Button';
 const Registration = () => {
   return (
-    <div>
-      <div></div>
-      <div>
-        <h1>Create a new account</h1>
-      
-        
-        <Link to="/login">Login</Link>
+
+<div className="SignUp_area">
+      <div className="SignUp-img">
+        <img src={login} alt="" />
+      </div>
+      <div className="signUp-info">
+        <h1 className="user-heading">Sign Up</h1>
+
+        <div className="form">
+          <Form>
+          <Form.Group className="mb-3 input" controlId="formBasicEmail">    
+              <Form.Control className="input-control"  type="name" placeholder="User Name" />
+            </Form.Group>
+
+            <Form.Group className="mb-3 input" controlId="formBasicEmail">    
+              <Form.Control className="input-control"  type="email" placeholder="Enter email" />
+            </Form.Group>
+            
+            <Form.Group className="mb-3 input" controlId="formBasicPassword">
+              <Form.Control className="input-control"  type="password" placeholder="Password" />
+            </Form.Group>
+            <Form.Group className="mb-3 input" controlId="formBasicPassword">
+              <Form.Control className="input-control"  type="password" placeholder="Confirm Password" />
+            </Form.Group>
+            <Button className="button"   variant="primary" type="submit">
+              Sign Up
+            </Button>
+          </Form>
+        </div>
+        <div>
+            <span className="account">Please Login </span>
+                 <Link className="user-join" to="/login">Login</Link>
+        </div>
+        <div className="icons-list">
+            <p className="icon-text">or Login With </p>
+            <span ><Button className="direct-link">Google</Button></span>
+            <span ><Button className="direct-link">Facebook</Button></span>
+            <span ><Button className="direct-link">Github</Button></span>
+            
+            
+        </div>
       </div>
     </div>
+
+
   );
 };
 
