b22557791723d9041fcd6ea66cea28ced7c80a6d
Eity2021 <Eity2021akter@gmail.com>
2022-07-14 21:34:39 +0600

    responsive navbar

--- src/Car-services/Shared/Header/Header.js
+++ src/Car-services/Shared/Header/Header.js
@@ -1,24 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import logo from "../../images/logo.png";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
 const Header = () => {
-  return (
-    <div className="header_area">
-      <div className="header_logo">
-        <Link to="/">
-          <img src={logo} alt="" />
-        </Link>
-      </div>
-      <div className="header_link">
+  const [open, setOpen] = useState(false);
 
-        <Link className="link" to="/">Home</Link>
-        <Link className="link" to="about">About</Link>
-        <Link className="link" to="service">Service</Link>
-        <Link className="link" to="login">Login</Link>
-        
+  return (
+  
+      <div className="header_area">
+        <div onClick={() => setOpen(!open)}>
+          <FontAwesomeIcon className="fabars" icon={faBars}></FontAwesomeIcon>
+        </div>
+        <div className="header_logo">
+          <Link to="/">
+            <img src={logo} alt="" />
+          </Link>
+        </div>
+        <div className="color">
+          <div className={`header_link ${open ? "up" : "down"}`}>
+            <div className="header-linkup">
+              <Link className="link" to="/">
+                Home
+              </Link>
+              <Link className="link" to="about">
+                About
+              </Link>
+              <Link className="link" to="service">
+                Service
+              </Link>
+              <Link className="link" to="login">
+                Login
+              </Link>
+              <div onClick={() => setOpen(!open)}>
+                <FontAwesomeIcon
+                  className="faxmark"
+                  icon={faXmark}
+                ></FontAwesomeIcon>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
+   
   );
 };
 
